eeda710955e736dd1b7d69be55f2db1fce00b416
JD Barnhart <jd@jdbarnhart.com>
2008-06-27 10:49:30 -0700

    added hysteresis function to smooth

--- lib/plugins/smoother.rb
+++ lib/plugins/smoother.rb
@@ -6,16 +6,27 @@ class Smoother < ArduinoPlugin
   
   # directives, external variables and setup assignments and calls can be added rails style (not c style)
 
-# add to directives
-plugin_directives "#define ARY_SIZE 10"
-  
-# add to external variables
-external_variables "int *cur, *scan, *start, *end;", "int sm_ary[ARY_SIZE];"
+  # add to directives
+  plugin_directives "#define ARY_SIZE 10"
 
-# add the following to the setup method
-add_to_setup "scan = &sm_ary[0];", "cur = &sm_ary[0];", "start = &sm_ary[0];", "end = &sm_ary[ARY_SIZE-1];"
+  # add to external variables
+  external_variables "int *cur, *scan, *start, *end;", "int sm_ary[ARY_SIZE];", "int last_reading = 0;"
 
-# add an element to the array and return the average
+  # add the following to the setup method
+  add_to_setup "scan = &sm_ary[0];", "cur = &sm_ary[0];", "start = &sm_ary[0];", "end = &sm_ary[ARY_SIZE-1];"
+
+  # add an element to the array and return the average
+
+int add_hysteresis(int reading, int hysteresis)
+{
+  if ( ((reading - last_reading) > hysteresis) || ((last_reading - reading) > hysteresis)) {
+
+          last_reading = reading;
+          return reading;
+       }
+     else
+       return last_reading;
+}
 
 int smooth_average(int reading)
 {
@@ -27,11 +38,6 @@ int smooth_average(int reading)
   for (scan = &sm_ary[0]; scan < &sm_ary[ARY_SIZE-1]; cnt++, scan++)
     sum += *scan;
   ptr_reset();
-  Serial.print("smooth:sum: ");
-  Serial.print(sum);
-  Serial.print("smooth:cnt: ");
-  Serial.print(cnt);
-  Serial.print(" ");
   return sum/cnt;
 }
 

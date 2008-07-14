ae80119e96e4ccc908299011987c581324fcbe49
JD Barnhart <jd@jdbarnhart.com>
2008-07-14 08:33:24 -0700

    added Wire library for i2c -- needed to work around os x's file caching

---
+++ lib/examples/blink_m_hello.rb
@@ -0,0 +1,14 @@
+class BlinkMHello < ArduinoSketch
+
+  # just a scaffolding for blink_m_demo 
+
+  output_pin 19, :as => :wire, :device => :i2c, :enable => :true
+  
+  def loop
+
+	  x = 4    
+  
+  end
+    
+
+end

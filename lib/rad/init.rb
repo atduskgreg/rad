05814651e56d602717621f8dd528b925f6f5ef09
JD Barnhart <jd@jdbarnhart.com>
2008-07-17 08:44:44 -0700

    bugfix: fixed regex constant syntax in arduino_plugins

--- lib/rad/init.rb
+++ lib/rad/init.rb
@@ -5,7 +5,8 @@ unless defined?(PROJECT_DIR_NAME)
   PROJECT_DIR_NAME = a[a.length-1]
 end
 
-PLUGIN_C_VAR_TYPES = "int|void|unsigned|long|short|uint8_t|static"
+PLUGIN_C_VAR_TYPES = "int|void|unsigned|long|short|uint8_t|static|byte"
+
 
 %w(generators/makefile/makefile.rb rad_processor.rb rad_rewriter.rb variable_processing.rb arduino_sketch.rb arduino_plugin.rb tasks/rad.rb).each do |path|
   require File.expand_path("#{RAD_ROOT}/vendor/rad/#{path}")

5b54a2ae1bfc3ae81e7e8c04d2ef29a22116d3f6
JD Barnhart <jd@jdbarnhart.com>
2008-07-18 23:07:06 -0700

    added hello_eeprom example and i2c eeprom plugin

---
+++ lib/examples/hello_eeprom.rb
@@ -0,0 +1,54 @@
+class HelloEeprom < ArduinoSketch
+  
+  output_pin 19, :as => :rtc, :device => :i2c_ds1307, :enable => :true
+  output_pin 19, :as => :mem0, :device => :i2c_eeprom
+
+  output_pin 14, :as => :myLCD, :device => :pa_lcd, :rate => 19200
+    
+  def loop
+	    myLCD.home					# set to 0,0
+	    myLCD.print rtc.get(5, 1)	# refresh registers (=1) get month
+     	myLCD.print "/"
+	    myLCD.print rtc.get(4, 0)	# no need to refresh (=0) get day 
+     	myLCD.print "/"
+	    myLCD.print rtc.get(6, 0)	# get year
+     	myLCD.setxy(1,1)			# set in 1 byte line 1 (second line)
+     	printlz 2					# print hours with lead zero
+     	myLCD.print ":"
+	    printlz 1					# print minutes with lead zero
+     	myLCD.print ":"
+	    printlz 0					# print seconds with lead zero
+	    
+
+		myLCD.setxy 10,0
+		myLCD.print "write test"
+		myLCD.setxy 0,2
+		32.upto(109) do				# write address of byte to that b yte
+			|x| i2c_eeprom_write_byte  0x50, x, x
+			myLCD.print(".") if x%2
+			delay 10
+		end
+
+		delay 2000
+		
+		myLCD.setxy 0,2 	# clears bottom two lines           
+		myLCD.print "?l"
+		myLCD.setxy 0,3
+		myLCD.print "?l"
+
+		myLCD.setxy 10,0
+		myLCD.print "read test "
+		myLCD.setxy 0,2
+									# read and print 39 addresses with printable numbers
+		75.upto(113) { |x| myLCD.print(i2c_eeprom_read_byte(0x50, x)) }
+		delay 10000
+		myLCD.clearscr
+  end
+  
+  def	printlz(w)
+  			i = 0 + rtc.get(w,0)         # the '0 +' is YARH (Yet Another RubyToc Hack)
+  			myLCD.print "0" if i < 10
+  			myLCD.print i
+  end
+
+end

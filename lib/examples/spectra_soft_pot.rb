13cfda7eacddc85af701b5c613d3e5862826a814
JD Barnhart <jd@jdbarnhart.com>
2008-07-29 16:14:17 -0700

    minor edit to spectra_soft_pot example description

--- lib/examples/spectra_soft_pot.rb
+++ lib/examples/spectra_soft_pot.rb
@@ -1,12 +1,12 @@
 class SpectraSoftPot < ArduinoSketch
 
-  # demonstrate capibility to treat soft pot as traditional pot
-  # the pot reading remains "locked" to the last touch point
-  # same behavior as ipod
+  # demonstrate capability to use soft pot as traditional pot
+  # the last pot reading remains "locked" to the last touch point
+  # similar same behavior as ipod
   #
-  # this assumes a pa_lcd operating at 19200 and one 
-  # spectrasymbol softpot connected to analog pin 3
-  # which can be done with blink m address assignment
+  # this sketch assumes a pa_lcd operating at 19200 and one 
+  # spectra symbol softpot connected to analog pin 3
+  # 
 
   output_pin 5, :as => :my_lcd, :device => :pa_lcd, :rate => 19200, :clear_screen => :true
   input_pin 3, :as => :sensor_one, :device => :spectra
@@ -22,7 +22,7 @@ class SpectraSoftPot < ArduinoSketch
 
    def loop
      my_lcd.setxy 0,1
-     # since lcd's have issues with clearing tens and hundreds digits when reading ones, 
+     # since lcd's have issues clearing tens and hundreds digits when reading ones, 
      # we use pad_int_to_str, which is a hack to display these cleanly
      # pad_int_to_str takes two arguments: an integer and the final string length
      # 

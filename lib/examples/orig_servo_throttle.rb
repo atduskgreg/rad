f9e998295b7bfc23691ee569bf879e0d36bea10d
JD Barnhart <jd@jdbarnhart.com>
2008-07-31 12:34:50 -0700

    converted servo_throttle examples to new school instance variables in preparation for build_and_make update

--- lib/examples/orig_servo_throttle.rb
+++ lib/examples/orig_servo_throttle.rb
@@ -1,5 +1,8 @@
 class OrigServoThrottle < ArduinoSketch
   
+ ## revision 20080731
+ ## conversion to instance style variables
+  
      # old school servo control
      # uses pulsewidth for positioning
      # potentiometer to control servo
@@ -7,9 +10,13 @@ class OrigServoThrottle < ArduinoSketch
      # use analog pin for sensor
      # need to format the output of sensor_position and sensor_amount
      # probably going away soon
+     # 
 
-     
-     external_vars :sensor_position => "int, 0", :servo_amount => "int, 0", :pw => "int, 0", :time_check => "unsigned long, 0"
+
+     @sensor_position = 0
+     @servo_amount = 0
+     @pw = 0
+
 
      output_pin 5, :as => :my_lcd, :device => :sf_lcd
      input_pin 1, :as => :sensor
@@ -18,9 +25,9 @@ class OrigServoThrottle < ArduinoSketch
 
        def loop
          #delay 9 # comment out if using servo status, since it will add enough delay
-         sensor_position = analogRead(sensor)
-         servo_amount = (add_hysteresis(sensor_position, 10)*2 + 500)
-         move_servo my_servo, servo_amount
+         @sensor_position = analogRead(sensor)
+         @servo_amount = (add_hysteresis(@sensor_position, 10)*2 + 500)
+         move_servo my_servo, @servo_amount
          servo_status
          
        end
@@ -30,9 +37,9 @@ class OrigServoThrottle < ArduinoSketch
         my_lcd.setxy 0,0			# line 0, col 0
      		my_lcd.print "Read  Send"
      		my_lcd.setxy 0,1		# line 1, col 0
-     		my_lcd.print sensor_position # need method of blanking out previous reading
+     		my_lcd.print @sensor_position # need method of blanking out previous reading
      		my_lcd.setxy 6,1 
-     		my_lcd.print servo_amount
+     		my_lcd.print @servo_amount
        end
 
   
--- lib/examples/servo_throttle.rb
+++ lib/examples/servo_throttle.rb
@@ -1,12 +1,15 @@
 class ServoThrottle < ArduinoSketch
-  
+
+     # updated 20080731 
+     # replaced external variables with instance style variables
+     
      # potentiometer to control servo
      # with a bit of hysteresis
      # use analog pin for sensor
      # need to format the output of sensor_position and sensor_amount
 
-     
-     external_vars :sensor_position => "int, 0", :servo_amount => "int, 0"
+     @sensor_position = 0
+     @servo_amount = 0
 
      output_pin 5, :as => :my_lcd, :device => :sf_lcd
      input_pin 1, :as => :sensor
@@ -16,9 +19,9 @@ class ServoThrottle < ArduinoSketch
        def loop
          servo_refresh
          #delay 9 # comment out if using servo status, since it will add enough delay
-         sensor_position = analogRead(sensor)
-         servo_amount = (add_hysteresis(sensor_position, 10)*0.36)
-         my_servo.position servo_amount
+         @sensor_position = analogRead(sensor)
+         @servo_amount = (add_hysteresis(@sensor_position, 10)*0.36)
+         my_servo.position @servo_amount
          servo_status
          
        end
@@ -28,9 +31,9 @@ class ServoThrottle < ArduinoSketch
         my_lcd.setxy 0,0			# line 0, col 0
      		my_lcd.print "Read  Send"
      		my_lcd.setxy 0,1		# line 1, col 0
-     		my_lcd.print sensor_position # need method of blanking out previous reading
+     		my_lcd.print @sensor_position # need method of blanking out previous reading
      		my_lcd.setxy 6,1 
-     		my_lcd.print servo_amount
+     		my_lcd.print @servo_amount
        end
 
   

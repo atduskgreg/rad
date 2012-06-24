add7e51b509c4fe00344f2c542e38435b39f3cbf
Rogerio Medeiros <argerim@gmail.com>
2012-06-23 23:28:59 -0300

    Test fixed to run in any machine.

--- spec/models/sketch_compiler_spec.rb
+++ spec/models/sketch_compiler_spec.rb
@@ -35,11 +35,11 @@ context "SketchCompiler.new" do
     @as = File.expand_path(File.dirname(__FILE__)) + "/../../lib/examples/add_hysteresis.rb"
   end
   it "should correctly absolutize a path with /../ that starts at /" do
-    SketchCompiler.new(@as).path.should == "/Users/greg/code/rad/lib/examples/add_hysteresis.rb"
+    SketchCompiler.new(@as).path.should == FileUtils.pwd() + "/lib/examples/add_hysteresis.rb"
   end
   
   it "should correct absolutize a relative path" do
-    SketchCompiler.new("lib/examples/add_hysteresis.rb").path.should == "/Users/greg/code/rad/lib/examples/add_hysteresis.rb"
+    SketchCompiler.new("lib/examples/add_hysteresis.rb").path.should == FileUtils.pwd() + "/lib/examples/add_hysteresis.rb"
   end
   
   it "should load the body of the sketch" do
@@ -63,7 +63,7 @@ context "SketchCompiler#create_build_dir! without a path prefix" do
     @sc = SketchCompiler.new("lib/examples/add_hysteresis.rb")
   end
   it "should create the sketch dir in the correct place" do
-    @sc.should_receive( :mkdir_p ).with( "/Users/greg/code/rad/lib/examples/add_hysteresis" )
+    @sc.should_receive( :mkdir_p ).with( FileUtils.pwd() + "/lib/examples/add_hysteresis" )
     @sc.create_build_dir!
   end
 end
@@ -83,9 +83,8 @@ context "SketchCompiler#build_dir" do
     @sc = SketchCompiler.new("lib/examples/add_hysteresis.rb")
   end
   
-  
   it "should be correct with a default target_dir" do
-    @sc.build_dir.should == "/Users/greg/code/rad/lib/examples/add_hysteresis"
+    @sc.build_dir.should == FileUtils.pwd() + "/lib/examples/add_hysteresis"
   end
   
   it "should be correct with an altered target_dir" do
